7e3a3e4f039435b322ccddcc67c73792600d7f16
heejeong <hjx66@naver.com>
2022-12-08 00:24:35 +0900

    [Programmers] Level 1.모의고사 재풀이

--- Programmers/고득점Kit/완전탐색/모의고사.py
+++ Programmers/고득점Kit/완전탐색/모의고사.py
@@ -1,5 +1,53 @@
 '''
 프로그래머스 Level 1. 모의고사
+'''
+'''
+22.12.07 재풀이
+나의 idea :  정답 배열 완전 탐색하면서 score 계산
+- 각각 수포자 정답 패턴 미리 선언 >> i%len(answers)로 패턴 반복
+>> 최댓값 리턴
+'''
+
+def solution(answers):
+    answer1 = [1, 2, 3, 4, 5]
+    answer2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    score = [0,0,0]
+    for i in range(len(answers)):
+        if answer1[i%len(answer1)] == answers[i]:
+            score[0] += 1
+        if answer2[i%len(answer2)] == answers[i]:
+            score[1] += 1
+        if answer3[i%len(answer3)] == answers[i]:
+            score[2] += 1
+
+    arr_answer = []
+    max_value = max(score)
+    for i in range(3):
+        if max_value == score[i]:
+            arr_answer.append(i+1)
+    return arr_answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 나의 idea : 1,2,3번 답 배열 생성 후, for문 돌면서 정답배열이랑 비교 >> 맞출때마다 점수 증가 >> 가장 높은 값
 >> 통과
 But, 각 수포자의 답을 넣은 리스트 길이를 answers의 길이만큼 곱함 >> 쓸데없는 메모리 낭비 발생
