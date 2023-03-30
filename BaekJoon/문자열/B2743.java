c0f14eaee850f50623f2605a36a3aab661b58cab
hj-k66 <hjx66@naver.com>
2023-03-30 22:34:05 +0900

    [BOJ] 2743 단어 길이 재기

---
+++ BaekJoon/문자열/B2743.java
@@ -0,0 +1,11 @@
+package CodingTest.BaekJoon.문자열;
+
+import java.io.*;
+
+public class B2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.length());
+    }
+}
