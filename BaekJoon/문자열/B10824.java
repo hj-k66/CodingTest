116c210b9ba7c3dc7f0499dd229c5e1637ba29dd
hj-k66 <hjx66@naver.com>
2023-04-02 16:56:26 +0900

    [BOJ] 10824 네 수

---
+++ BaekJoon/문자열/B10824.java
@@ -0,0 +1,23 @@
+package CodingTest.BaekJoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+
+        String ab = a+b;
+        String cd = c+d;
+
+        System.out.println(Long.parseLong(ab) + Long.parseLong(cd));
+    }
+}
