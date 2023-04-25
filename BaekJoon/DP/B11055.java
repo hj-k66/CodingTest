221a6dfaf3e0586382634e79e145042e7995bf71
hj-k66 <hjx66@naver.com>
2023-04-26 00:01:45 +0900

    [BOJ] 11055 가장 큰 증가하는 부분 수열

---
+++ BaekJoon/DP/B11055.java
@@ -0,0 +1,30 @@
+package CodingTest.BaekJoon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n];
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[j]+arr[i], dp[i]);
+                }
+            }
+            answer = Math.max(dp[i], answer);
+        }
+        System.out.println(answer);
+    }
+}
