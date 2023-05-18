9f9a0381e87ddd3489dd26787662ee4ee50e6175
hj-k66 <hjx66@naver.com>
2023-05-18 21:44:49 +0900

    [BOJ] 1476 날짜 계산

---
+++ BaekJoon/완전탐색/B1476.java
@@ -0,0 +1,36 @@
+package CodingTest.BaekJoon.완전탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int e = 1; int s = 1; int m = 1;
+        for(int year = 1;;year++){
+            if(e == E && s == S && m == M){
+                System.out.println(year);
+                break;
+            }
+            e++;
+            s++;
+            m++;
+            if(e == 16){
+                e = 1;
+            }
+            if(s == 29){
+                s = 1;
+            }
+            if(m == 20){
+                m = 1;
+            }
+        }
+    }
+
+}
